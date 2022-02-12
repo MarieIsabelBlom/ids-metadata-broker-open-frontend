02681647cdecf6c938e753f318158dc78f74b8ab
lakdev <l.kratky@lakdev.de>
2022-02-12 11:56:34 +0100

    footer adjustments

--- searchfrontend/src/components/Footer.js
+++ searchfrontend/src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
             <Box component="footer" className="footer" m={4}>
                 <Grid container className="footer-copyright">
                     <Grid container item xs={6}  lg={3} md={3} className="footer-header">
-                        <h5 style={{ fontSize: '22px', marginTop: '16px' }}>Mobility<br />Data Space</h5>
+                        <h5 style={{ fontSize: '22px', marginTop: '16px', fontWeight: 'normal' }}>Mobility<br />Data Space</h5>
                         <p style={{ fontSize: '18px', textAlign: 'left', color: '#8F8F8F' }}>International Data Spaces</p>
                     </Grid>
                     <Grid className="footer-mail" container item xs={6} lg={3} md={3} >
@@ -32,7 +32,8 @@ class Footer extends Component {
                     </Grid>
                     <Grid className="footer-right" item xs={6} lg={3} md={3}  >
                         <p className="modified">  Last Modified: {new Date(document.lastModified).getDate() + "." + parseInt(new Date(document.lastModified).getMonth() + 1) + "." + new Date(document.lastModified).getFullYear()}</p>
-                        <a className="copyright" href="https://www.iais.fraunhofer.de/.org/">© {new Date().getFullYear()}&nbsp;Fraunhofer IAIS</a>
+                        <p>&nbsp;</p>
+                        <p><a className="copyright" href="https://www.iais.fraunhofer.de/.org/">© {new Date().getFullYear()}&nbsp;Fraunhofer IAIS</a></p>
                     </Grid>
 
                 </Grid>
